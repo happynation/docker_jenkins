07f5768539fdd848b38ff5e3e94272dae113efc1
vovatran1993 <vovatran1993@gmail.com>
2019-03-24 15:51:39 -0500

    jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,15 @@
+node{
+    properties([parameters([string(defaultValue: '127.0.0.2', description: 'Please specify IP', name: 'ip', trim: true)])])
+    stage("Remove container"){
+        try{
+            sh "ssh root@${IP} docker rm -f Flaskex"
+        }
+        catch(exc){
+            sh "echo container deleted"
+        }
+    }
+    
+    stage("Run container"){
+        sh "ssh root@${IP} docker run -d --name Flaskex -p 6000:4000 vovatran/flaskex"
+    }
+}
